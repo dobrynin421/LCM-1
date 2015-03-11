84c9b1c1925b33e23b2b7e8d6915faa45c695255
Dina <hdina@inbox.ru>
2015-03-11 11:29:55 +0600

    Турция

---
+++ Turkey/123.R
@@ -0,0 +1,13 @@
+setwd("E:/")
+mydata<-read.table(file = "Turkey.txt", header = TRUE)
+attach(mydata)
+plot(Year, xlab="Год",terrorism, ylab="Терроризм", main=("Частота упоминаний характеристики для Турции"), pch=15, col="red", type="b", xlim=c(2000,2014),ylim=c(0,700))
+lines (Year,terrorist,xlim=c(2000,2014),pch=16,col="green",type="b")
+lines (Year,occupation,xlim=c(2000,2014),pch=17,col="violet",type="b")
+lines (Year,narcotic,xlim=c(2000,2014),pch=18,col="black",type="b")
+lines (Year,violation,xlim=c(2000,2014),pch=19,col="blue",type="b")
+lines (Year,democracy,xlim=c(2000,2014),pch=20,col="grey",type="b")
+lines (Year,development,xlim=c(2000,2014),pch=21,col="orange",type="b")
+legend ("topleft" ,inset = 0.01,title = "Характеристика",c("терроризм","террорист","оккупация","наркотики","насилие","демократия","развитие"),lty =c(1,1,1,1,1,1,1),pch=c(15,16,17,18,19,20,21),col=c("red","green","violet","black","blue","grey","orange"))
+
+
